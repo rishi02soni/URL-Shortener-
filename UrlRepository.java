c781c5ca88c92d2ce014c90052e3709b807db679
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-25 22:17:53 +0530

    Create UrlRepository.java

---
+++ UrlRepository.java
@@ -0,0 +1,10 @@
+package com.project.urlshortener.repository;
+
+import com.project.urlshortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortUrl(String shortUrl);
+}
